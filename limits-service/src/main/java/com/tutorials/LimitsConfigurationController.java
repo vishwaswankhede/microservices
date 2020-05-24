a2f6e7f9e9016e6f9850c8612627e4353db9e9be
vishwaslaxman.w <vishwaslaxman.w@NAGSEZ206805L.ad.infosys.com>
2020-05-24 15:47:56 +0530

    update

--- limits-service/src/main/java/com/tutorials/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/tutorials/LimitsConfigurationController.java
@@ -4,17 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.tutorials.bean.LimitConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
-	
+
 	@Autowired
 	private Configuration configuration;
-	
 
 	@GetMapping("/limits")
 	public LimitConfiguration retriveLimitsFromConfigurations() {
-		return new LimitConfiguration( configuration.getMaximum(), configuration.getMinimum());
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 99);
 	}
 }
